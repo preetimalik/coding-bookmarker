c9f8b4a567d4539aa21c88fe493d813ab14e4e9a
Preeti <pmalik@mtu.edu>
2018-02-07 09:33:07 -0600

    added validation

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ function saveBookmark(e){
 	var siteName = document.getElementById('siteName').value;
 	var siteURL = document.getElementById('siteURL').value;
 
+	if(!validateForm(siteName, siteURL)){
+
+		return false;
+	}
+
 	var bookmark = {
 		name: siteName,
 		url: siteURL
@@ -24,9 +29,40 @@ else{
 	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 	bookmarks.push(bookmark);
 	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-}	
+}
+
+	// Clear form
+	document.getElementById('myForm').reset;
+
+	fetchBookmarks();
+
 // Prevent form from submitting
 	e.preventDefault();
+}
+
+// Delete bookmark
+
+function deleteBookmark(url) {
+
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+	for(var i = 0; i < bookmarks.length; i++){
+
+		if(bookmarks[i].url == url){
+
+			// Remove bookmark from array 
+
+			bookmarks.splice(i, 1);
+
+		}
+	}
+		// Reset the local storage
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+	// Fetch bookmarks again
+
+	fetchBookmarks();
+
 }
 
 function fetchBookmarks(){
@@ -47,10 +83,38 @@ function fetchBookmarks(){
 
 		bookmarkResults.innerHTML += '<div class="well">' +
 									 '<h3>'+ name +
+									 '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
+									 '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="'+url+'">Delete</a>'+
 									 '</h3>' +
 									 '</div>';
 	}
 
 
 
+}
+
+
+
+// Validate Form
+
+
+function validateForm(siteName, siteURL){
+
+
+	if(!siteName || !siteURL){
+
+		alert('Please fill in the form before saving!');
+		return false;
+	}
+
+	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	var regex = new RegExp(expression);
+
+	if(!siteURL.match(regex)){
+
+		alert('Please use a valid URL!');
+		return false;
+	}
+
+	return true;
 }
